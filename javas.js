b405081679e855cd31934fa1199381fc55ec2363
Ankit Yadav <officealankit2007@gmail.com>
2026-09-07 22:49:06 +0530

    Update typed.js strings for multiple roles

--- javas.js
+++ javas.js
@@ -59,7 +59,7 @@ ScrollReveal().reveal('.home-content .heading',{ origin: 'top'});
 
 /*=========================typed  js ==========================*/
 const typed = new Typed('.multiple-text',{
-    strings: ['Frontend Developer', 'Frontend Developer'],
+    strings: ['Frontend Developer', 'full stack Developer','Data Anlistick'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
